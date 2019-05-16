59ae4ca174d334746b6523ba32198923f6244ece
Lauren Boyer <lboyer4@outlook.com>
2019-05-16 09:45:16 -0600

    fetch reservations from api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+
+    }
+  }
+
+  componentDidMount = () => {
+    const url = 'http://localhost:3001/api/v1/reservations' ;
+    const reservations = fetch(url)
+    .then(response => response.json())
+    .then(results => this.setReservations(results))
+    .catch(error => console.log('error fetching reservations'))
+  }
+
+  setReservations = (reservations) => {
+    this.setState({reservations})
+  }
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,7 +35,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <CardContainer reservatiosn={this.state.reservations} />
         </div>
       </div>
     )
